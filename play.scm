2ba65cd3c3d20a99e89023c2cb369e7507a7bd3a
Jesse Wattenbarger <jesse.j.wattenbarger@gmail.com>
2021-01-02 10:26:01 -0500

    Add some more stuff from the little schemer to play.scm

--- play.scm
+++ play.scm
@@ -73,3 +73,47 @@
     (and (subset? set1 set2)
          (subset? set2 set1))))
 
+(define intersect?
+  (lambda (set1 set2)
+    (cond
+     ((null? set1) #f)
+     ((member? (car set1) set2) #t)
+     (else
+      (intersect? (cdr set1) set2)))))
+
+;; There are more to do here from ch 07
+;; But, I'm messing with ch 08 now
+
+;; The seqL fn is what insertL does that insertR doesn't
+(define seqL
+  (lambda (new old l)
+    (cons new (cons old l))))
+;; The seqR fn is what insertR does that insertL doesn't
+(define seqR
+  (lambda (new old l)
+    (cons old (cons new l))))
+
+;; insert-g acts as insertL or insertR depending on the helper fn
+(define insert-g
+  (lambda (seq)
+    (lambda (new old l)
+      (cond
+       ((null? l) '())
+       ((eq? (car l) old)
+        (seq new old (cdr l)))
+       (else
+        (cons (car l) ((insert-g seq) new old (cdr l))))))))
+
+;; Build insertL with insert-g and seqL
+(define insertL (insert-g seqL))
+;; Build insertR with insert-g and seqR
+(define insertR (insert-g seqR))
+
+;; The seqS fn is what subst does that insertL and insertR don't
+(define seqS
+  (lambda (new old l)
+    (cons new l)))
+;; Build subst with (insert-g seqS)
+(define subst (insert-g seqS))
+
+
